89ec76cacb98f4447a31fcde35e82d68ac4cba79
Larry Masullo <lmasullo@gmail.com>
2020-02-19 15:24:59 -0600

    API working

--- app/App.svelte
+++ app/App.svelte
@@ -1,79 +1,80 @@
 <script context="module">
   // Dependencies
-  import { fetch } from "tns-core-modules/fetch";
+  //import { fetch } from "tns-core-modules/fetch";
+  const httpModule = require("tns-core-modules/http");
 
-  export async function preload(page, session) {
-    const testJson = ["One", "Two", "Three"];
-    return testJson;
-    //const url = "https://reporganizer.herokuapp.com/api/repos";
-    // let result = await fetch(url);
-    //return repos;
+  export async function preload() {
+    // httpModule.getJSON("https://reporganizer.herokuapp.com/api/repos").then(
+    //   r => {
+    //     console.log(r);
+    //     repos = r;
+    //     return { repos };
+    //   },
+    //   e => {
+    //     console.log("Error getting Repos", e);
+    //   }
+    // );
   }
 </script>
 
 <script>
-  //import { fetch } from "tns-core-modules/fetch";
+  // Declare variable to hold repos
+  let repos;
 
-  export let testJson;
-
-  // const testJson = ["One", "Two", "Three"];
-  // const test = "Yo!";
-  const test2 = testJson[1];
-
-  //export let repos;
-  //const url = "https://reporganizer.herokuapp.com/api/repos";
-
-  //   async function responseOrDie(url) {
-  //     let response = await fetch(url);
-  //     console.log(response);
-
-  //     return response;
-  //   }
-
-  //let repos = responseOrDie(url);
-
-  //let testJson1;
-
-  //   $: testJson1 = testJson[0];
-
-  let counter = 42;
-  let message;
-  let message2;
-  $: message =
-    counter <= 0
-      ? "Hoorraaay! You unlocked the Svelte-Native clicker achievement!"
-      : `${counter} taps left`;
-
-  const onTap = () => counter--;
+  // let message;
 
   async function getRepos() {
-    // let result = await responseOrDie(url);
-    // const url = "https://reporganizer.herokuapp.com/api/repos";
+    console.log("Inside getRepos!!");
 
-    // let result = await fetch(url);
-    // console.log("Result", result);
-    //message2 = result[0].name;
-    console.log("Inside getRepos 2!!");
-    // message2 = result.length;
-    //message2 = repos.length;
-    message2 = test2;
+    // API Call
+    httpModule.getJSON("https://reporganizer.herokuapp.com/api/repos").then(
+      r => {
+        repos = r;
+        //return { repos };
+        //console.log(r);
+        //let test = document.getElementById("btnRepos");
+        let test = Button.getViewById("btnRepos");
+        test.text = "Hi!";
+      },
+      e => {
+        console.log("Error getting Repos", e);
+      }
+    );
   }
+
+  function makeInvis(args) {
+    //let test = Button.getViewById("btnRepos");
+    //test.text = "Hi!";
+    const page = args.object;
+    let sl = page.getViewById("btnRepos");
+    sl.visibility = "collapsed";
+  }
+
+  //getRepos();
 </script>
 
 <page>
-  <actionBar title="My App Demo 23" icon="" />
+  <actionBar title="Liberamed Demo" icon="" />
   <stackLayout class="p-20">
-    <label text="Tap the button" class="h1 text-center" />
-    <button text="TAP" on:tap={onTap} class="-primary -active" />
-    <label text={message} class="h2 text-center" textWrap="true" />
-    <!-- <label text={test2} class="h2 text-center" textWrap="true" /> -->
+    <label text="Tap to Get Repos" class="h1 text-center" />
 
-    <!-- {#each repos as item}
-      <label text={item.name} class="h2 text-center" textWrap="true" />
-    {/each} -->
+    <!-- <label text={message} class="h2 text-center" textWrap="true" /> -->
 
-    <button text="Get Repos" on:tap={getRepos} class="-primary -active" />
-    <label text={message2} class="h2 text-center" textWrap="true" />
+    <button
+      id="btnRepos"
+      text="Get Repos"
+      on:tap={getRepos}
+      class="-primary -active" />
+
+    <button text="Make hidden" on:tap={makeInvis} class="-primary" />
+
+    {#if repos}
+      {#each repos as item}
+        <label text={item.name} class="h2 text-center" textWrap="true" />
+      {/each}
+    {:else}
+      <label text="No Repos" class="h2 text-center" textWrap="true" />
+    {/if}
 
   </stackLayout>
 </page>
