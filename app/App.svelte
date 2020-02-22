96580dcdabdcae652447be2528d83ba5dad594a0
Lawrence N. Masullo <lmasullo@gmail.com>
2020-02-22 06:55:01 -0600

    Fetch works

--- app/App.svelte
+++ app/App.svelte
@@ -1,6 +1,16 @@
 <script context="module">
   // Dependencies
   //import { fetch } from "tns-core-modules/fetch";
+  // Create the Observable Module, listen for changes on the home page
+  const observableModule = require("tns-core-modules/data/observable")
+    .Observable;
+
+  // Get the application module
+  const application = require("tns-core-modules/application");
+
+  // Create the new viewModel, will listen to the home page
+  const viewModel = new observableModule();
+
   const httpModule = require("tns-core-modules/http");
 
   export async function preload() {
@@ -18,6 +28,29 @@
 </script>
 
 <script>
+  //  Set the Modules
+
+  // Preferences page loads - Use Load because Navigate to only happens once, loaded happens each time you go to the page
+  function onPageLoaded(args) {
+    console.log("Preferences - On Loaded");
+
+    // Declare the Page Object
+    const page = args.object;
+
+    //const getRepoBtn = page.getViewById("btnRepos");
+
+    //const page = args.object;
+    const getRepoBtn = page.getViewById("lblTest");
+
+    let makeInvis = false;
+
+    if (makeInvis) {
+      console.log(makeInvis);
+
+      getRepoBtn.visibility = "collapsed";
+    }
+  }
+
   // Declare variable to hold repos
   let repos;
 
@@ -30,11 +63,11 @@
     httpModule.getJSON("https://reporganizer.herokuapp.com/api/repos").then(
       r => {
         repos = r;
-        //return { repos };
-        //console.log(r);
-        //let test = document.getElementById("btnRepos");
-        let test = Button.getViewById("btnRepos");
-        test.text = "Hi!";
+        // return { repos };
+        // console.log(r);
+        // let test = document.getElementById("btnRepos");
+        // let test = Button.getViewById("btnRepos");
+        // test.text = "Hi!";
       },
       e => {
         console.log("Error getting Repos", e);
@@ -43,22 +76,22 @@
   }
 
   function makeInvis(args) {
-    //let test = Button.getViewById("btnRepos");
-    //test.text = "Hi!";
     const page = args.object;
-    let sl = page.getViewById("btnRepos");
-    sl.visibility = "collapsed";
+    const getRepoBtn = page.getViewById("btnRepos");
+    getRepoBtn.visibility = "collapsed";
+    console.log("Invisible");
   }
 
   //getRepos();
 </script>
 
-<page>
+<page
+  xmlns="http://schemas.nativescript.org/tns.xsd"
+  loaded="onPageLoaded"
+  class="page">
   <actionBar title="Liberamed Demo" icon="" />
   <stackLayout class="p-20">
-    <label text="Tap to Get Repos" class="h1 text-center" />
-
-    <!-- <label text={message} class="h2 text-center" textWrap="true" /> -->
+    <label id="lblTest" text="Tap to Get Repos" class="h1 text-center" />
 
     <button
       id="btnRepos"
@@ -66,7 +99,7 @@
       on:tap={getRepos}
       class="-primary -active" />
 
-    <button text="Make hidden" on:tap={makeInvis} class="-primary" />
+    <!-- <button text="Make hidden" on:tap={makeInvis} class="-primary" /> -->
 
     {#if repos}
       {#each repos as item}
